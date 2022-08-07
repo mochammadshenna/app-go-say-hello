cd73d612670625eb8fef4d1d7fb906689887172e
mochammadshenna <shenawardana@gmail.com>
2022-08-07 18:55:38 +0700

    First commit my apps with go

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/mochammadshenna/app-go-say-hello
+
+go 1.18
+
+require github.com/mochammadshenna/go-say-hello v1.0.0
---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+
+	go_say_hello "github.com/mochammadshenna/go-say-hello"
+)
+
+func main() {
+	// go_say_hello.SayHello()
+
+	fmt.Println(go_say_hello.SayHello())
+}
+
+// Output:
+// Hello, world!
+//
+// END OMIT
+
+// START OMIT
+// Language: go
+// Path: main.go
